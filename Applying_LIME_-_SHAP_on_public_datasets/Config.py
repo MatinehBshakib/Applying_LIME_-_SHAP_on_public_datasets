4d8a7bb466a0cc3e73e128d4ce3857d28e8fc676
MatinehBshakib <6028035@studenti.unige.it>
2026-01-22 02:02:51 +0100

    add config file

---
+++ Applying_LIME_-_SHAP_on_public_datasets/Config.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+import numpy as np 
+
+class MycordinalConfig:
+      
+      Hierarchy_mapping= {
+            "mech_cols": ["OTEK_LANC", "RAZRIV", "ZSN"],
+            "elec_cols": ["FIBR_JELUD", "JELUD_TAH", "A_V_BLOK", "FIBR_PREDS", "PREDS_TAH"],
+            "chem_cols": ["P_IM_STEN", "REC_IM", "DRESSLER"]
+      }
+      
