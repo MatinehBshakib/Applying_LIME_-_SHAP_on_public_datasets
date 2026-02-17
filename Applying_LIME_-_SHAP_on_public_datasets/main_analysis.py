243dc074e7182e57c75f143526f4d46f8493cb98
MatinehBshakib <6028035@studenti.unige.it>
2026-02-17 14:56:10 +0100

    add main file to rank the features and apply spearman and jaccard on datasets

---
+++ Applying_LIME_-_SHAP_on_public_datasets/main_analysis.py
@@ -0,0 +1,28 @@
+from Analysis import XAIComparativeAnalysis
+import os
+
+def main():
+      analysis = XAIComparativeAnalysis()
+      base_folder = 'outputs'
+      # 1. Update this list with ALL your file names
+      files_to_process = [
+      'Breast_Cancer.csv', 
+      'CDC_Diabetes.csv',
+      'Cervical_Cancer.csv', 
+      'Diabetes1999-2008.csv',
+      'Diabetic_Retinopathy_Debrecen.csv',
+      'Glioma_Grading_Clinical.csv',
+      'Hepatit_C_Virus.csv',
+      'Myocardinal_Infraction.csv',
+      'Obesity_level.csv',
+      ]
+      files_to_process = [os.path.join(base_folder, f) for f in files_to_process]
+      # 2. Run the Loop
+      for f in files_to_process:
+            analysis.execute_analysis(f)
+
+      # 3. Save the Final Table
+      analysis.save_final_table(output_folder=base_folder)
+
+if __name__ == "__main__":
+    main()
